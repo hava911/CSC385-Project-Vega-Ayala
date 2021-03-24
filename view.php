1d4be788f922fa929588e3089773473e3ec6885b
HAVA9 <72930980+HAVA9@users.noreply.github.com>
2021-03-24 14:22:05 -0400

    Update view.php

--- view.php
+++ view.php
@@ -6,156 +6,87 @@ File Name: view.php
 Purpose: To display all the products and categories as well to sort through it.
 -->
 
-<?php require_once("header.html"); ?> 
+<?php
 
-        <link rel="stylesheet" type="text/css" href="assets/css/slide.css">
-            <!-- begining of our menu -->
-            <div class="container">
-               
-      
-               
-               <h1> Shirts </h1>
-               
-               <div class="hover panel" onmouseout="javascript:this.className = 'hover panel';" onmouseover="javascript:this.className += ' flip';">
-                    <div class="front">
-                        <div class="frontTitle">
-                            The Jordan Shirt
-                        </div>
-                        <div class="frontLogo twice-fleming bookBgImg"></div>
-                        <div class="frontLocation">
-                            	Air Highness
-                        </div>
-                    </div>
-                    <div class="back">
-                        <div class="backTitle">
-                           The Jordan Shirt $15.00
-                        </div>
-                        <div class="backParagraph">
-                            
-                            <form>
-                            
-                            <input type="radio" id="small" name="size" value="small">
-  				<label for="small">Small </label><br>
-  			     <input type="radio" id="medium" name="size" value="medium">
-				<label for="medium">  Medium</label><br>
-			     <input type="radio" id="large" name="size" value="large">
-  				<label for="large">   Large </label> <br>
-  				<input type="radio" id="xlarge" name="size" value="xlarge">
-  				<label for="lxarge">  X-Large </label> <br>
-  			<label for="line">---------------------------</label> <br>	
-  				
-                         
-  				
-  				
-  				<label for="qty"><i></i> Quantity</label>
-                            <input type="number" id="qty" name="qty" placeholder="Quantity">
-                            	<label for="notes"><i></i> Notes</label>
-                            <input type="text" id="notes" name="notes" placeholder="Any notes?">
-  				
-  				</form>
-  				
-                        </div>
-                        <div class="backGoto">
-                            <a href="#" target="_blank" title="..."><h1>Add To Cart</h1></a>
-                        </div><h1>
-                    </div>
-                </div>
-               
-                
-                
-                <div class="hover panel" onmouseout="javascript:this.className = 'hover panel';" onmouseover="javascript:this.className += ' flip';">
-                    <div class="front">
-                        <div class="frontTitle">
-                            The Nike Shirt
-                        </div>
-                        <div class="frontLogo sherlock-doyle bookBgImg"></div>
-                        <div class="frontLocation">
-                            	Just Do It
-                        </div>
-                    </div>
-                    <div class="back">
-                        <div class="backTitle">
-                            The Nike Shirt $15.00
-                        </div>
-                        <div class="backParagraph">
-                           
-                           <form>
-                            
-                            <input type="radio" id="small" name="size" value="small">
-  				<label for="small">Small </label><br>
-  			     <input type="radio" id="medium" name="size" value="medium">
-				<label for="medium">  Medium</label><br>
-			     <input type="radio" id="large" name="size" value="large">
-  				<label for="large">   Large </label> <br>
-  				<input type="radio" id="xlarge" name="size" value="xlarge">
-  				<label for="lxarge">  X-Large </label> <br>
-  				
-  			<label for="line">---------------------------</label> <br>	
 
-  				
-  				
-  				<label for="qty"><i></i> Quantity</label>
-                            <input type="number" id="qty" name="qty" placeholder="Quantity">
-                            	<label for="notes"><i></i> Notes</label>
-                            <input type="text" id="notes" name="notes" placeholder="Any notes?">
-  				
-  				</form>
-                           
-                        </div>
-                        <div class="backGoto">
-                            <a href="#" target="_blank" title="..."><h1>Add To Cart</h1></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="hover panel" onmouseout="javascript:this.className = 'hover panel';" onmouseover="javascript:this.className += ' flip';">
-                    <div class="front">
-                        <div class="frontTitle">
-                            The Champion Shirt
-                        </div>
-                        <div class="frontLogo rebellion-morgan bookBgImg"></div>
-                        <div class="frontLocation">
-                            We are the Champions!
-                        </div>
-                    </div>
-                    <div class="back">
-                        <div class="backTitle">
-                           The Champion Shirt $15.00
-                        </div>
-                        <div class="backParagraph">
-                          
-                          <form>
-                            <input type="radio" id="small" name="size" value="small">
-  				<label for="small">Small </label><br>
-  			     <input type="radio" id="medium" name="size" value="medium">
-				<label for="medium">  Medium</label><br>
-			     <input type="radio" id="large" name="size" value="large">
-  				<label for="large">   Large </label> <br>
-  				<input type="radio" id="xlarge" name="size" value="xlarge">
-  				<label for="lxarge">  X-Large </label> <br>
-                          
-  			<label for="line">---------------------------</label> <br>	
-  	
-  				
-  				<label for="qty"><i></i> Quantity</label>
-                            <input type="number" id="qty" name="qty" placeholder="Quantity">
-                            	<label for="notes"><i></i> Notes</label>
-                            <input type="text" id="notes" name="notes" placeholder="Any notes?">
-  				
-  				</form>
-                           
-                        </div>
-                        <div class="backGoto">
-                            <a href="#" target="_blank" title="..."><h1>Add To Cart<h1></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-              
-      
+$db = mysqli_connect('localhost','hv18624_shirts','Reading@10','hv18624_shirts');
+$errmsg = $db->connect_error ;
+if ($errmsg != null) echo "Error: $errmsg" ;
+else
+  {
+
+
+$query="SELECT * FROM Products WHERE productCategory = 'Shirt' ";
+ $res = $db->query($query);
+
+ if ($res)
+{
+
+require_once("header.html");  
+
+     echo '   <link rel="stylesheet" type="text/css" href="assets/css/slide.css"> ';
+            
+      echo '      <div class="container"> ';
                
+              /* require_once("../categories.html"); */
+               
+       echo '        <h1> Shirts </h1> ';
+
+
+
+while($row = mysqli_fetch_assoc($res))
+{
+
+		
+		include("flipFirstLine.html");
+		
+               echo '     <div class="front"> ';
+               echo '     <div class="frontTitle"> ' . $row['productName'] .  ' </div> ';
+               echo '        <div class="frontLogo ' . $row['picture'] . ' bookBgImg"></div> ';
+               echo '         <div class="frontLocation"> ' . $row['description'] . ' </div> ';
+               echo '     </div> ';
+     
+               echo '    <div class="back"> ';
+               echo '    <div class="backTitle">'  . $row['productName'] . '</div> ';
+               echo '    <div class="backParagraph"> ';
                             
-            <!-- ending of our menu -->
-	
-<?php require_once("footer.html"); ?>
+               echo '       <form> ';
+                            
+               echo '        <input type="radio" id="small" name="size" value="small"> ';
+  	       echo '		<label for="small">Small $' . $row['priceReg'] . '.00 </label><br> ';
+  	       echo '	     <input type="radio" id="medium" name="size" value="medium"> ';
+	       echo '    	<label for="medium">Medium $' . $row['priceReg'] . '.00 </label><br> ';
+	       echo '	     <input type="radio" id="large" name="size" value="large"> ';
+  	       echo '		<label for="large">Large  $' . $row['priceReg'] . '.00 </label> <br> ';
+  	       echo '	     <input type="radio" id="xLarge" name="size" value="xLarge"> ';
+  	       echo '		<label for="xLarge"> X Large  $' . $row['priceXlarge'] . '.00 </label> <br> ';
+  				
+  	       echo '	<label for="line">---------------------------</label> <br> ';	
+ 		
+ 	
+  		echo '		<label for="qty"><i></i> Quantity</label> ';
+                echo '           <input type="number" id="qty" name="qty" placeholder="Quantity"> ';
+                echo '         	<label for="notes"><i></i> Notes</label> ';
+                echo '           <input type="text" id="notes" name="notes" placeholder="Any notes?"> ';
+  				
+                echo '    </div> ';
+                echo '      <div class="backGoto"> <a href="#" target="_blank" title="..."><h1>Add To Cart</h1></a> </div> ';
+                echo '		</form> ';
+                echo '    </div> ';
+                echo '    </div> ';
+
+
+
+
+
+}
+}
+
+$db->close();
+
+}
+
+
+require_once("footer.html"); 
+
+?>
